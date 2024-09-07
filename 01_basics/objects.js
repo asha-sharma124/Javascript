45a8b6d42c3ffb440af053ebec926425c6c9e51a
Asha sharma <144535182+asha-sharma124@users.noreply.github.com>
2024-09-07 10:23:39 +0000

    objects in JS

---
+++ 01_basics/objects.js
@@ -0,0 +1,73 @@
+//singleton: when made from constructor than it is used
+//object literals
+
+//Object.create
+// let mysym=Symbol("key1")
+
+
+// const user={
+//     name:"asha",
+//     Id:2,
+//     [mysym]:"mk1",
+//     "full name":"asha sharma", 
+//     email:"asha@gmail.com",
+//     lastlogindays:["monday","tuesday","saturday"]
+
+// }
+// console.log(user.name)
+// console.log(user.lastlogindays[0])
+// console.log(user["email"])
+// console.log(typeof user[mysym])
+
+// Object.freeze(user)//after this no one can chnge the value of id
+// user.Id=1
+// console.log(user.Id)
+
+
+
+// user.greeting=function(){
+//     console.log(`congratulations ${user.name}`)//or this.name
+// }
+
+// console.log(user.greeting())
+
+//**************************Singleton Objects****************** */
+
+
+// const user=new Object();
+// console.log(user)
+
+const userone={
+   Id:1
+    ,fullname:{
+        firstname:"asha",
+        lastname:"sharma"
+    },age:18
+}
+// console.log(userone.fullname?.firstname)
+
+
+
+const obj1={
+    1:"a",
+    2:"b"
+}
+const obj2={
+    3:"c",
+    4:"d"
+}
+
+const obj3={obj1,obj2}
+//console.log(obj3)
+//const obj5=Object.assign(obj1,obj2) in this both object will combine and the output will dlflect in obj1 so bad practise
+const obj4=Object.assign({},obj1,obj2)//we applied curly nbracket so that output will deflect and add in new object and not in obj1]
+const obj6={...obj1,...obj2}
+console.log(obj4)
+console.log(obj6["1"])
+
+
+console.log(Object.keys(userone));
+console.log(Object.values(userone));
+console.log(Object.entries(userone));
+
+console.log(userone.hasOwnProperty("firstname"))
