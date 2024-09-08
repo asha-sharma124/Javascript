3a3dbb63ea67102a852ebb82bc39e99d751b7906
Asha sharma <144535182+asha-sharma124@users.noreply.github.com>
2024-09-08 09:38:08 +0000

    loops in JS

---
+++ 01_basics/loops.js
@@ -0,0 +1,21 @@
+let score =1;
+do{
+    console.log("hello")
+}while(score<0)//will work one time minimum for sure
+
+
+//for of loop
+const array=[1,2,3,4,5]
+for (const i of array) {
+    console.log(i)
+    
+}
+const string="hello world"
+for(let i of string){
+    console.log(`element is ${i}`)
+}
+
+
+///Maps 
+const map= new Map();//does not have duplicate value and have unique values
+
